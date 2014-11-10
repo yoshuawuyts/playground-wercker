f4ed2685706c0d5c91286df9bcd2aeb054b36133
yoshuawuyts <i@yoshuawuyts.com>
2014-11-10 13:23:43 +0100

    we need http

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var assert = require('assert');
 var request = require('request');
 console.log(process.argv);
 
-request(process.argv[2], function (error, response, body) {
+request('http://' + process.argv[2], function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
   }
